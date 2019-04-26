c9c768f6bc52c629322519c059e01d1d609fd4ad
Jade Michael Thornton <jade@jmthornton.net>
2019-04-26 11:18:46 -0500

    use pragmatic style

--- options.js
+++ options.js
@@ -3,13 +3,13 @@ const pkg = require('./package.json');
 const eslint = require('eslint');
 
 module.exports = {
-  cmd: 'pragmatic',
-  version: pkg.version,
-  homepage: pkg.homepage,
-  bugs: pkg.bugs.url,
-  eslint,
-  tagline: 'Clean, modern and correct code',
-  eslintConfig: {
-    configFile: path.join(__dirname, 'eslintrc.json')
-  }
+	cmd: 'pragmatic',
+	version: pkg.version,
+	homepage: pkg.homepage,
+	bugs: pkg.bugs.url,
+	eslint,
+	tagline: 'Clean, modern and correct code',
+	eslintConfig: {
+		configFile: path.join(__dirname, 'eslintrc.json')
+	}
 };
