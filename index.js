eaed05cd477e82c91f11fb15bd3174e6855f10bf
Jade Michael Thornton <jade@jmthornton.net>
2019-04-26 11:05:31 -0500

    convert to global package

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-'use strict';
-
-module.exports = {
-  env: {
-    es6: true,
-    node: true,
-    browser: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2019,
-  },
-  extends: 'eslint:recommended',
-  plugins: [
-    'promise',
-    'no-iife'
-  ],
-  rules: {
-    // first reduce some recommended levels
-    'no-console': 'warn',
-    'no-debugger': 'warn',
-    'no-mixed-spaces-and-tabs': 'off',
-    'no-octal': 'off',
-
-    // now load our rules
-    ...require('./rules/detect-problems'),
-    ...require('./rules/optimization'),
-    ...require('./rules/modern-js'),
-    ...require('./rules/promises'),
-    ...require('./rules/clean-code')
-  },
-};
-
-console.log(module.exports);
+const Linter = require('standard-engine').linter;
+const opts = require('./options');
+module.exports = new Linter(opts);
---
+++ options.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const pkg = require('./package.json');
+const eslint = require('eslint');
+
+module.exports = {
+  cmd: 'pragmatic',
+  version: pkg.version,
+  homepage: pkg.homepage,
+  bugs: pkg.bugs.url,
+  eslint,
+  tagline: 'Clean, modern and correct code',
+  eslintConfig: {
+    configFile: path.join(__dirname, 'eslintrc.json')
+  }
+};
